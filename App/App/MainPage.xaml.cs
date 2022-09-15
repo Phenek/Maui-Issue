62e5bcdaacc142094eb1c78746e6fd20cb933fad
Clovis Gonon <cgonon@globe-group.com>
2022-09-15 18:58:16 +0200

    - issue #11204

---
+++ App/App/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace App;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnPushClicked(object sender, EventArgs e)
+    {
+        VisualStateManager.GoToState(_border1, $"State{count}");
+        VisualStateManager.GoToState(_border2, $"State{count}");
+        ++count;
+        if (count > 5)
+            count = 1;
+    }
+}
+
