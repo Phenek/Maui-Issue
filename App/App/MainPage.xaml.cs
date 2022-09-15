cf204e5c535d5b357795f1292e8e537b917bb221
Clovis Gonon <cgonon@globe-group.com>
2022-09-15 18:58:16 +0200

    - issue #10543

---
+++ App/App/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace App;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnPushClicked(object sender, EventArgs e)
+    {
+        Application.Current.MainPage.Navigation.PushModalAsync(new MainPage2());
+    }
+}
+
---
+++ App/App/MainPage2.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace App;
+
+public partial class MainPage2 : ContentPage
+{
+	int count = 0;
+
+	public MainPage2()
+	{
+		InitializeComponent();
+	}
+
+	private void OnPopClicked(object sender, EventArgs e)
+	{
+        Application.Current.MainPage.Navigation.PopModalAsync();
+    }
+
+    private void OnRemoveClicked(object sender, EventArgs e)
+    {
+        Application.Current.MainPage.Navigation.RemovePage(this);
+    }
+}
+
