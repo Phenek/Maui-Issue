8a720358bf29c409dd98259881182d1de45858d3
Clovis Gonon <cgonon@globe-group.com>
2022-09-15 18:58:16 +0200

    - issue #8489

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ScrollConflicts;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		_carouselView.ItemsSource = new List<ContentView>
+			{
+				new Tab1(),
+				new Tab2(),
+				new Tab3()
+			};
+	}
+}
+
+
